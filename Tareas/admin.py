521ab4ef72952c858a5bb1e4792743c610b2b814
Agustin Migliorero <agustinmigliorero@hotmail.com>
2023-10-19 19:42:37 -0300

    Mi primer commit!

---
+++ Tareas/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Proyecto, Item
+
+admin.site.register(Proyecto)
+admin.site.register(Item)
+
+# Register your models here.
---
+++ Tareas/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+
+def Tareas(request):
+    return HttpResponse("<h1>Holaaa!!</h1>")
